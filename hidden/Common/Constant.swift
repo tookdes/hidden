bd19a02f741478d82dd7ed4f56cddceac62d9f35
tooktang <tooktang@gmail.com>
2026-06-06 21:10:33 +0800

    fix: use packaged app icon

--- hidden/Common/Constant.swift
+++ hidden/Common/Constant.swift
@@ -12,5 +12,5 @@ enum Constant {
     static let appName = "Hidden Bar"
     static let launcherAppId = "com.dwarvesv.LauncherApplication"
     
-    private(set) static var isUsingLTRLanguage = false
+    static var isUsingLTRLanguage = false
 }
